0c04d48049a48cd1ba8c66ee63cfcb4255b7e026
Itamar Kalimi <itamarkalimi@gmail.com>
2023-11-09 23:05:00 +0200

    day 15 23:00

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 'use strict'
 var gBoard
 
-var gameCell = {
-    minesAroundCount: 0,
-    isShown: false,
-    isMine: false,
-    isMarked: false
-}
+var MINE = 'X'
 
 var gLevel = {
     size: 4,
@@ -21,9 +16,16 @@ var gGames = {
 }
 
 function onInit() {
+    //document.querySelector('.cell').addEventListener("click", onFirstTimeClick())
+    //gBoard = onFirstTimeClick()
     gBoard = buildBoard()
-    console.log(gBoard)
+    console.log('gBoard buildBoard:', gBoard)
+    // console.log('gBoard2 setMinesNegsCount:', gBoard)
     renderBoard(gBoard)
+    //addMines(gBoard)
+    // saif 4 test:
+    //console.log('renderMines', renderMines())
+    //console.log('getRandomEmptyCellPosition:', getRandomEmptyCellPosition())
 }
 
 function buildBoard() {
@@ -32,10 +34,15 @@ function buildBoard() {
         gameBoard[i] = []
         for (var j = 0; j < gLevel.size; j++) {
             // create cell 
-            gameBoard[i][j] = gameCell
-            //console.log(gameCell)
+            gameBoard[i][j] = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
         }
     }
+
     gameBoard[0][1] = {
         minesAroundCount: 0,
         isShown: false,
@@ -66,12 +73,13 @@ function buildBoard() {
 
 function renderBoard(board) {
 
-
     var strHTML = ''
     for (var i = 0; i < board.length; i++) {
         strHTML += `<tr class="game-row" >\n`
         for (var j = 0; j < board[0].length; j++) {
             const cell = board[i][j]
+            //var cellClass = getClassName({ i, j })
+
             // add class to cell
             var className = (cell.isMine && cell.isShown) ? 'mine-cell' : ''
             strHTML += `\t<td class="cell ${className}" 
@@ -80,10 +88,14 @@ function renderBoard(board) {
         }
         strHTML += `</tr>\n`
     }
+
+
     console.log(strHTML)
-    setMinesNegsCount(board)
+    setMinesNegsCount(gBoard)
     const elGameTable = document.querySelector('.game-table')
     elGameTable.innerHTML = strHTML
+
+    //console.log('addMines', addMines())
 
 }
 
@@ -96,7 +108,8 @@ function setMinesNegsCount(board) {
             console.log(`board[${i}][${j}].minesAroundCount:`, board[i][j].minesAroundCount)
         }
     }
-
+    console.log('setMinesNegsCount gBoard: ', gBoard)
+    return board
 }
 
 // count the mines around the cell
@@ -109,10 +122,7 @@ function countMinesAroundCount(rowIdx, colIdx, mat) {
         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= mat[i].length) continue
-            if (mat[i][j]) {
-                if (mat[i][j].isMine === true) minesAroundCount++
-            }
-
+            if (mat[i][j].isMine === true) minesAroundCount++
         }
     }
     console.log('minesAroundCount: ', minesAroundCount)
@@ -131,29 +141,83 @@ function onCellClicked(elCell, i, j) {
     // change style of cell 
 }
 
-function onCellClickedTyota(elCell, i, j) {
-    const cell = gCinema[i][j]
-    // ignore none seats and booked
-    if (!cell.isSeat || cell.isBooked) return
 
-    console.log('Cell clicked: ', elCell, i, j)
 
-    // Support selecting a seat
-    elCell.classList.add('selected')
+// // saif 4 - bug
+// function addMines(board) {
+//     const location = getRandomEmptyCellPosition(board)
+//     if (!location) return
+//     // change the i,j location to mine
+//     board[location.i][location.j].isMine = true
+//     console.log('addMines location: ', board[location.i][location.j])
+//     renderMines(location, MINE)
+//     return location
+// }
 
-    if (gElSelectedSeat) {
-        gElSelectedSeat.classList.remove('selected')
+// // saif 4
+// // Convert a location object {i, j} to a selector and render a value in that element
+
+// function renderMines(location, value) {
+//     const cellSelector = "." + getClassName(location)
+//     console.log('renderMines cellSelector', cellSelector)
+//     const elCell = document.querySelector(cellSelector)
+//     elCell.innerHTML = value
+// }
+
+// // saif 4
+
+// function getRandomEmptyCellPosition(board) {
+//     const emptyCells = []
+//     for (var i = 0; i < board.length; i++) {
+//         for (var j = 0; j < board[i].length; j++) {
+//             const cell = board[i][j]
+//             if (!cell.isMine) {
+//                 emptyCells.push({ i, j })
+//             }
+//         }
+//     }
+
+//     if (!emptyCells.length) return null
+
+//     const randIdx = getRandomInt(0, emptyCells.length)
+//     console.log('getRandomEmptyCellPosition randIdx:', randIdx)
+//     return emptyCells[randIdx]
+// }
+// // saif 4
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min)
+//     max = Math.floor(max)
+//     return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+// }
+
+// // saif 4
+// // Returns the class name for a specific cell
+
+// function getClassName(position) {
+//     const cellClass = `cell-${position.i}-${position.j}`
+//     console.log('getClassName cellClass', cellClass)
+//     return cellClass
+// }
+
+//saif 6
+
+function onFirstTimeClick() {
+    const gameBoard = []
+    for (var i = 0; i < gLevel.size; i++) {
+        gameBoard[i] = []
+        for (var j = 0; j < gLevel.size; j++) {
+            // create cell 
+            gameBoard[i][j] = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
+        }
     }
 
-    // Only a single seat should be selected
-    gElSelectedSeat = (gElSelectedSeat !== elCell) ? elCell : null
-
-    // When seat is selected a popup is shown
-    if (gElSelectedSeat) {
-        showSeatDetails({ i, j })
-    } else {
-        hideSeatDetails()
-    }
+    document.querySelector('.cell').removeEventListener("click", onFirstTimeClick())
+    return gameBoard
 }
 
 function onCellMarked(elCell) {
